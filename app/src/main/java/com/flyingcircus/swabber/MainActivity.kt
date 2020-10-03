99b36b6da49467e1a990c86cc10b8e420c2714fb
Gal-Kinberg <gankl33@gmail.com>
2020-10-03 18:54:21 +0300

    Remove all tests

--- app/src/main/java/com/flyingcircus/swabber/MainActivity.kt
+++ app/src/main/java/com/flyingcircus/swabber/MainActivity.kt
@@ -7,11 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    // Check
-//    Hello Kobi
-    // Hi there
-    // Bye
-    fun printHello() { println("Hello") }
 
+    }
 }
