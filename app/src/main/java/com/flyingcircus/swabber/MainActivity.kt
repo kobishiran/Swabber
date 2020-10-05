a11e2bd9f609408cd92c9e903c0c4c33d55b368c
Kobi Shiran <kobijshiran@gmail.com>
2020-10-05 23:05:43 +0300

    Add Interactivity Graphics

--- app/src/main/java/com/flyingcircus/swabber/MainActivity.kt
+++ app/src/main/java/com/flyingcircus/swabber/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.flyingcircus.swabber
 
 import android.graphics.Color
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     // Initialise global variables
     val initialSickNum = 10         // number of "mines"
-    val boardHeight = 10             // number of rows
+    val boardHeight = 16             // number of rows
     val boardWidth = 10             // number of columns
     var gameBoard = Array(boardHeight) { row -> Array<Person>(boardWidth) { col -> Person(row, col) } }
     var unknownCounter = boardHeight * boardWidth // number of "tiles" not "exposed" neither "flagged"
@@ -25,8 +28,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        printInitBoard(boardHeight,boardWidth)
+        initializeBoard()
+        displayInitBoard(boardHeight,boardWidth)
         startTimer(dayLengthMilli)
+        clickListener(boardHeight,boardWidth)
     }
 
     override fun onDestroy() {
@@ -34,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
-    fun initializeBoard() {
+    private fun initializeBoard() {
         // wipe board clean
         gameBoard = Array(boardHeight) { row -> Array<Person>(boardWidth) { col -> Person(row, col) } }
 
@@ -47,17 +52,20 @@ class MainActivity : AppCompatActivity() {
         randomIndices.forEach { index -> gameBoard[index / boardWidth][index % boardWidth].isSick = true }
     }
 
-    private fun printInitBoard(boardHeight: Int, boardWidth: Int){
+    private fun displayInitBoard(boardHeight: Int, boardWidth: Int){
         // The size of matrix
-        val spaceX = 0   // spacing between elements in each row
-        val spaceY = 0   // spacing between elements in each column
+        val spaceX = 10   // spacing between elements in each row
+        val spaceY = 10   // spacing between elements in each column
 
 
-        for (counterY in 1..boardHeight) {
+        for (row in 1..boardHeight) {
 
-            // Create new horizontal LinearLayout programmatically
+            // Create new horizontal LinearLayout, named "child", programmatically
             val child = LinearLayout(this)
             child.orientation = LinearLayout.HORIZONTAL
+
+            // Set unique id to each child (100, 200, 300, ... , 100 * boardHeight)
+            child.id = 100 * row
 
             // Create a LinearLayout.LayoutParams object for the new horizontal LinearLayout
             val layoutParams = LinearLayout.LayoutParams(
@@ -74,13 +82,17 @@ class MainActivity : AppCompatActivity() {
 
 
             // Now in the new horizontal layout create n-text views
-            for (counterX in 1..boardWidth) {
+            for (col in 1..boardWidth) {
 
                 // Create a new TextView instance programmatically
                 val imageview = ImageView(this)
 
+                // Set unique id to each imageview (101, 102, ..., 100+width, 201, 202, ... ... , 100* height + width)
+                imageview.id = 100 * row + col
+
                 // Set Image Resource
-                imageview.setImageResource(R.mipmap.ic_launcher)
+                imageview.setImageResource(R.drawable.unexposed)
+
 
                 // Create a LinearLayout.LayoutParams object for text view
                 val imageParams = LinearLayout.LayoutParams(
@@ -102,26 +114,45 @@ class MainActivity : AppCompatActivity() {
                 imageview.setPadding(0, 0, 0, 0)
 
 
-                // Finally, add the text view to the view group
+                // Add the text view to the view group (horizontal LinearLayout, "child")
                 child.addView(imageview)
 
 
-                // Increment counterX
+                // Increment col
             }
 
-
+            // Add the "child" view to the view group (vertical LinearLayout "root_layout")
             root_layout.addView(child)
 
-            // Increment counterY
+            // Increment row
         }
     }
 
-    fun clickTile(row: Int, col: Int) {
+    private fun clickListener(boardHeight: Int, boardWidth: Int){
+        for (row in 1..boardHeight) {
+            for (col in 1..boardWidth) {
+                val temp: ImageView = findViewById(100 * row + col)
+                  temp.setOnClickListener() {
+                      Toast.makeText(this,"you clicked on $row and $col", Toast.LENGTH_SHORT).show()
+                      //clickTile(row, col)
+                  }
+                temp.setOnLongClickListener() {
+                    Toast.makeText(this,"you holded on $row and $col", Toast.LENGTH_SHORT).show()
+                    //holdTile(row, col)
+                    return@setOnLongClickListener true
+                }
+            }
+        }
+    }
+
+    private fun clickTile(row: Int, col: Int) {
         // Check if the tile is already exposed or has mask
         if (gameBoard[row][col].isExposed) {
             // TODO: show error: Already exposed!
+            Toast.makeText(this,"Already exposed!", Toast.LENGTH_SHORT).show()
         } else if (gameBoard[row][col].hasMask) {
             // TODO: show error: has a mask!
+            Toast.makeText(this,"Has a mask!", Toast.LENGTH_SHORT).show()
         } else {
             exposeTile(row, col)
             checkVictory()
@@ -168,7 +199,7 @@ class MainActivity : AppCompatActivity() {
         if (row - 1 >= 0 && col - 1 >= 0)                   gameBoard[row - 1][col - 1].isInfectable = false
     }
 
-    private fun countNeighbors(row: Int, col: Int): Int {
+    private  fun countNeighbors(row: Int, col: Int): Int {
         var contactNumber = 0
         if (row + 1 < boardHeight && col + 1 < boardWidth) contactNumber += gameBoard[row + 1][col + 1].isSick.toInt()
         if (row + 1 < boardHeight) contactNumber                         += gameBoard[row + 1][col].isSick.toInt()
@@ -181,7 +212,7 @@ class MainActivity : AppCompatActivity() {
         return contactNumber
     }
 
-    fun holdTile(row: Int, col: Int) {
+    private fun holdTile(row: Int, col: Int){
         if (!gameBoard[row][col].isExposed) { // make sure the tile is not already exposed
             when (gameBoard[row][col].hasMask) {
                 true -> {  // if already has mask, remove it and increment mask counter
@@ -196,6 +227,7 @@ class MainActivity : AppCompatActivity() {
                         unknownCounter--
                     } else {
                         // TODO: Show error: Not enough masks!
+                        Toast.makeText(this,"Not enough masks!", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
@@ -203,15 +235,43 @@ class MainActivity : AppCompatActivity() {
 
         } else {
             // TODO: Show error: Already exposed!
+            Toast.makeText(this,"Already exposed!", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun updateDisplay(row: Int, col: Int) {
-        // TODO("Not yet implemented")
+
+        // go to the unique id of that element
+        val temp: ImageView = findViewById(100 * row + col)
+
+        if(gameBoard[row][col].hasMask){
+            temp.setImageResource(R.drawable.unexposedflagged)
+        } else if (!gameBoard[row][col].isExposed)
+            temp.setImageResource(R.drawable.unexposed)
+          else if (gameBoard[row][col].isExposed) {
+            when (gameBoard[row][col].cantactNumber){
+                0 -> temp.setImageResource(R.drawable.exposed0)
+                1 -> temp.setImageResource(R.drawable.exposed1)
+                2 -> temp.setImageResource(R.drawable.exposed2)
+                3 -> temp.setImageResource(R.drawable.exposed3)
+                4 -> temp.setImageResource(R.drawable.exposed4)
+                5 -> temp.setImageResource(R.drawable.exposed5)
+                6 -> temp.setImageResource(R.drawable.exposed6)
+                7 -> temp.setImageResource(R.drawable.exposed7)
+                8 -> temp.setImageResource(R.drawable.exposed8)
+                9 -> temp.setImageResource(R.drawable.exposed9)
+            }
+            if(gameBoard[row][col].isSick)
+                temp.setImageResource(R.drawable.exposedbomb)
+        }
     }
 
     private fun gameOver(victory: Boolean) {
         // TODO("Not yet implemented")
+        if(victory)
+            Toast.makeText(this,"Winner!", Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(this,"Loser!", Toast.LENGTH_SHORT).show()
     }
 
     fun checkVictory() {
@@ -221,7 +281,7 @@ class MainActivity : AppCompatActivity() {
     // extension function to turn bool to int
     fun Boolean.toInt() = if (this) 1 else 0
 
-    fun startTimer(timeToCountInMilli: Long) {
+    private fun startTimer(timeToCountInMilli: Long) {
         countDownTimer = object : CountDownTimer(timeToCountInMilli, 1000L) {
 
             // every second, update the timer textView
