81f49ec9cf430e0624fd4109f09e8e6e5cb8cd91
Gal-Kinberg <gankl33@gmail.com>
2020-10-06 16:22:58 +0300

    Game Over Screens
    Added basic game over screens. They do not terminate the game yet.

---
+++ app/src/main/java/com/flyingcircus/swabber/WinScreen.kt
@@ -0,0 +1,11 @@
+package com.flyingcircus.swabber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WinScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_win_screen)
+    }
+}
