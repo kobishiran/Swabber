6378683449b53c3585b0f667d0c1b11aea8ee8b6
Kobi Shiran <kobijshiran@gmail.com>
2020-10-07 01:57:49 +0300

    Add home screen layout + improve Winner&Loser layout

---
+++ app/src/main/java/com/flyingcircus/swabber/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.flyingcircus.swabber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+
+
+}
---
+++ app/src/main/java/com/flyingcircus/swabber/HomeScreenAnimation.kt
@@ -0,0 +1,16 @@
+package com.flyingcircus.swabber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreenAnimation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen_animation)
+    }
+
+    fun displayAnimation(){
+        // TODO make opening animation - virus couphing
+    }
+
+}
